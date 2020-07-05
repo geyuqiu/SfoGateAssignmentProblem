530fcee521f6366ab10437c5bf73f87bad107ff8
Yuqiu Ge <yge@it-economics.de>
2020-07-05 14:41:13 +0200

    closes #6.: setup primeng

--- jhipster/src/main/webapp/app/app.module.ts
+++ jhipster/src/main/webapp/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
 import './vendor';
-import { SfoGateAssignmentProblemSharedModule } from 'app/shared/shared.module';
-import { SfoGateAssignmentProblemCoreModule } from 'app/core/core.module';
-import { SfoGateAssignmentProblemAppRoutingModule } from './app-routing.module';
-import { SfoGateAssignmentProblemHomeModule } from './home/home.module';
-import { SfoGateAssignmentProblemEntityModule } from './entities/entity.module';
+import {SfoGateAssignmentProblemSharedModule} from 'app/shared/shared.module';
+import {SfoGateAssignmentProblemCoreModule} from 'app/core/core.module';
+import {SfoGateAssignmentProblemAppRoutingModule} from './app-routing.module';
+import {SfoGateAssignmentProblemHomeModule} from './home/home.module';
+import {SfoGateAssignmentProblemEntityModule} from './entities/entity.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
-import { MainComponent } from './layouts/main/main.component';
-import { NavbarComponent } from './layouts/navbar/navbar.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
-import { ErrorComponent } from './layouts/error/error.component';
+import {MainComponent} from './layouts/main/main.component';
+import {NavbarComponent} from './layouts/navbar/navbar.component';
+import {FooterComponent} from './layouts/footer/footer.component';
+import {ActiveMenuDirective} from './layouts/navbar/active-menu.directive';
+import {ErrorComponent} from './layouts/error/error.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+	  BrowserAnimationsModule,
     SfoGateAssignmentProblemSharedModule,
     SfoGateAssignmentProblemCoreModule,
     SfoGateAssignmentProblemHomeModule,
