250e987fb69a84f5228325625a4112b1f2a035e5
Yuqiu Ge <yge@it-economics.de>
2020-07-07 21:59:53 +0200

    #34: display bars in different colors

--- jhipster/src/main/webapp/app/shared/constants/bar.constants.ts
+++ jhipster/src/main/webapp/app/shared/constants/bar.constants.ts
@@ -1,6 +1,9 @@
 import {ChartData, ChartOptions} from 'chart.js';
 
 export const chartOptions: ChartOptions = {
+	legend: {
+		display: false
+	},
 	scales: {
 		yAxes: [{
 			scaleLabel: {
@@ -15,8 +18,11 @@ export const chartDataTemplate: ChartData = {
 	labels: ['1', '2', '3', 'I'], // static
 	datasets: [
 		{
-			label: '#DEP - #ARR',
-			backgroundColor: '#435565',
+			backgroundColor: ['rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)'
+			],
 			data: []
 		}
 	]
